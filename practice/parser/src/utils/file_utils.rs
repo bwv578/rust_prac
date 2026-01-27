7fcd69482d59228b0daed51c069d10a8f1ffe29d
smh <bwv578g@gmail.com>
2026-01-27 21:07:06 +0900

    feat: DelimitedIter - 문자열 무시 옵션 추가

--- practice/parser/src/utils/file_utils.rs
+++ practice/parser/src/utils/file_utils.rs
@@ -6,16 +6,19 @@ use std::io::{BufReader, Error, Read, BufWriter};
 pub struct DelimitedIter {
     out: (String, Option<char>),
     delimiters: HashSet<char>,
+    chars_to_ignore: HashSet<char>,
     reader: Option<BufReader<File>>
 }
 
 impl DelimitedIter {
+
     pub fn new(file_path:&String) -> Result<DelimitedIter, Error> {
         let file_to_read:File = File::open(file_path)?;
         return Ok(
             Self {
                 out: (String::new(), None),
                 delimiters: HashSet::new(),
+                chars_to_ignore: HashSet::new(),
                 reader: Some(BufReader::new(file_to_read))
             }
         )
@@ -28,12 +31,20 @@ impl DelimitedIter {
         return self;
     }
 
+    pub fn set_chars_to_ignore(&mut self, chars: &[char]) -> &mut Self {
+        self.chars_to_ignore.clear();
+        for ig_char in chars {
+            self.chars_to_ignore.insert(*ig_char);
+        }
+        return self;
+    }
+
     pub fn next(&mut self) -> Option<(String, Option<char>)> {
         self.out = (String::new(), None);
         let mut buff = [0u8; 1];
+        let reader = self.reader.as_mut().expect("No file reader");
 
         loop {
-            let reader = self.reader.as_mut().expect("No file reader");
             match reader.read(&mut buff) {
                 Ok(0) => {
                     if self.out.0.is_empty() {
@@ -47,7 +58,7 @@ impl DelimitedIter {
                     if self.delimiters.contains(&c) {
                         self.out.1 = Some(c);
                         return Some(std::mem::take(&mut self.out));
-                    }else{
+                    }else if !self.chars_to_ignore.contains(&c) {
                         self.out.0.push(c);
                     }
                 }
